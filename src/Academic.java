4e85d9b7d2a4ce1a962b1c8e43846a77c7fe78d2
Akshay Kheterpal <akshay@Akshays-MacBook-Air.local>
2026-03-19 03:41:51 -0400

    fixed issue with import java.util.Date to make Date attribute work. unsure if we should use import java.time.LocalDate; instead.

--- src/Academic.java
+++ src/Academic.java
@@ -1,9 +1,10 @@
+import java.util.Date;
 public class Academic extends Event{
     public String subject;
     public String speaker;
 
 
-    public Academic(int eventId, String name, String date, int maxParticipants, Organizer organizer, String subject, String speaker) {
+    public Academic(int eventId, String name, Date date, int maxParticipants, Organizer organizer, String subject, String speaker) {
         super(eventId, name, date, maxParticipants, organizer);
         this.subject = subject;
         this.speaker = speaker;
--- src/Community.java
+++ src/Community.java
@@ -1,8 +1,9 @@
+import java.util.Date;
 public class Community extends Event{
     public String purpose;
 
 
-    public Community(int eventId, String name, String date, int maxParticipants, Organizer organizer, String purpose) {
+    public Community(int eventId, String name, Date date, int maxParticipants, Organizer organizer, String purpose) {
         super(eventId, name, date, maxParticipants, organizer);
         this.purpose= purpose;
     }
