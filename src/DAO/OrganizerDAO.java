956c6639d8db2818edd53bd4ea0ff5bd934c08ca
Akshay Kheterpal <akshay@Akshays-MacBook-Air.local>
2026-04-02 20:10:58 -0400

    started EventDAO and ParticipantDAO using template StudentDAO from class as a guide.

---
+++ src/DAO/OrganizerDAO.java
@@ -0,0 +1,5 @@
+package DAO;
+
+public class OrganizerDAO {
+    //    add, getall, getbyid, update
+}
---
+++ src/Exception/EventFullException.java
@@ -0,0 +1,11 @@
+package Exception;
+
+public class EventFullException extends Exception {
+
+public EventFullException() { } //constructor
+
+public EventFullException(String message)
+{
+    super(message);
+}
+}
