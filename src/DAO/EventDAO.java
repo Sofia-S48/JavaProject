2f81075d9d0327c0a8063bdff6ec1ec20bf1733d
Akshay Kheterpal <akshay@Akshays-MacBook-Air.local>
2026-04-03 17:08:37 -0400

    worked on EventDAO and Participant DAO. left some functions out in EventDAO because i didn't really undestand. will ask for help from sofia or rahman later

--- src/DAO/EventDAO.java
+++ src/DAO/EventDAO.java
@@ -1,7 +1,10 @@
 package DAO;
 
+import Model.Academic;
 import Model.Event;
 import DB.DbConnection;
+import Model.Registration;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -55,4 +58,114 @@ public class EventDAO {
             return null;
         }
     }
+        public void removeEvent(int eventId) throws SQLException{
+            try
+            {
+                String query = "DELETE FROM Event WHERE eventId = ?";
+
+                Connection myConnection = DbConnection.getConnection();
+
+                if(myConnection != null)
+                {
+                    PreparedStatement myQuery = myConnection.prepareStatement(query);
+                    myQuery.setInt(1, eventId);
+
+                    int rowsDelete = myQuery.executeUpdate();
+
+                    if(rowsDelete >0)
+                    {
+                        System.out.println("Event deleted");
+                    }
+                    else
+                    {
+                        System.out.println("Event not found.");
+                    }
+                }
+            }
+            catch (Exception e1)
+            {
+                e1.printStackTrace();
+            }
+
+        }
+            //need help because event is an abstract and i don't understand what is going wrong.
+//        public void updateEvent(Event e) throws SQLException {
+//            try {
+//                String query = "UPDATE Event SET name = ?, date = ?, location = ? WHERE event_id = ?";
+//
+//                Connection myConnection = DbConnection.getConnection();
+//
+//                if (myConnection != null) {
+//                    PreparedStatement statement = myConnection.prepareStatement(query);
+//
+//                }
+//            }
+//        }
+        public List<Event> getAllEvents() throws SQLException
+        {
+            String query = "SELECT * FROM Event";
+
+            Connection myConnection = DbConnection.getConnection();
+
+            if(myConnection != null)
+            {
+                try{
+                    PreparedStatement statement = myConnection.prepareStatement(query);
+                    ResultSet eventResults= statement.executeQuery(query);
+
+                    List<Event> allEvents = new ArrayList<>();
+
+                    while(eventResults.next())
+                    {
+                        int eventId = eventResults.getInt("event_id");
+                        String name = eventResults.getString("name");
+                        java.util.Date Date = eventResults.getDate("date");
+                        String location = eventResults.getString("location");
+
+                        //need help.
+                        Event ev =new event(ev.getEventId(), ev.getName()); //*not done. i don't understand what to do since event is abstract.
+                        allEvents.add(ev);
+                    }
+                    return allEvents;
+                }
+                catch (Exception e1)
+                {
+                    e1.printStackTrace();
+                }
+            }
+            return null;
+        }
+        public Event getById(int eventId) throws SQLException {
+            try {
+                String query = "SELECT * FROM Event WHERE event_id = ?";
+
+                Connection myConnection = DbConnection.getConnection();
+
+                if (myConnection != null) {
+                    PreparedStatement statement = myConnection.prepareStatement(query);
+                    statement.setInt(1, eventId);
+
+                    ResultSet eventResults = statement.executeQuery(query);
+
+
+                    if (eventResults.next()) {
+                        int evId = eventResults.getInt("event_id");
+                        String name = eventResults.getString("name");
+                        java.util.Date date = eventResults.getDate("date");
+                        String location = eventResults.getString("location");
+
+                        Event ev = new //** (); //don't know what to do because event is abstract.
+                        return ev;
+
+                    }
+                }
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+
+            return null;
+        }
+
+
+
 
--- src/DAO/ParticipantDAO.java
+++ src/DAO/ParticipantDAO.java
@@ -2,6 +2,7 @@ package DAO;
 
 import DB.DbConnection;
 import Model.Participant;
+import Model.Registration;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -58,4 +59,91 @@ public class ParticipantDAO {
             }
             return null;
         }
+    public List<Participant> getAllParticipants() throws SQLException
+    {
+        String query = "SELECT * FROM Partipant";
+
+        Connection myConnection = DbConnection.getConnection();
+
+        if(myConnection != null)
+        {
+            try{
+                PreparedStatement statement = myConnection.prepareStatement(query);
+                ResultSet participantResults= statement.executeQuery(query);
+
+                List<Participant> allParticipants = new ArrayList<>();
+
+                while(participantResults.next())
+                {
+                    int id = participantResults.getInt("participantId");
+                    String name = participantResults.getString("participantName");
+                    String contact = participantResults.getString("contactInformation");
+
+                    Participant participant1 = new Participant(0, null, null, 0, null, id, name, contact);
+                    allParticipants.add(participant1);
+                };
+                return allParticipants;
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+    public Participant getById(int participantId) throws SQLException {
+        try {
+            String query = "SELECT * FROM Participant WHERE participantId = ?";
+
+            Connection myConnection = DbConnection.getConnection();
+
+            if (myConnection != null) {
+                PreparedStatement statement = myConnection.prepareStatement(query);
+                statement.setInt(1, participantId);
+
+                ResultSet participantResults = statement.executeQuery(query);
+
+
+                if (participantResults.next()) {
+                    int id = participantResults.getInt("participantId");
+                    String name = participantResults.getString("participantName");
+                    String contact = participantResults.getString("contactInformation");
+
+                    ///  *** NOT DONE NEED HELP HERE.
+
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    public void removeParticipant (int participantId) throws SQLException{
+        try{
+            String query = "DELETE FROM Participant WHERE participantId = ?";
+
+            Connection myConnection = DbConnection.getConnection();
+
+            if(myConnection != null){
+                PreparedStatement myQuery = myConnection.prepareStatement(query);
+                myQuery.setInt(1, participantId);
+
+                int rowsDelete = myQuery.executeUpdate();
+
+                if(rowsDelete >0)
+                {
+                    System.out.println("Participant deleted");
+                }
+                else
+                {
+                    System.out.println("Participant not found.");
+                }
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
