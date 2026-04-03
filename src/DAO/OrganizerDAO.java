3e2c147e373ad40adcff42fc81216efe106c28d6
Akshay Kheterpal <akshay@Akshays-MacBook-Air.local>
2026-04-03 15:07:22 -0400

    saving current AOOP local changes i made

--- src/DAO/OrganizerDAO.java
+++ src/DAO/OrganizerDAO.java
@@ -1,7 +1,20 @@
 package DAO;
 
+import DB.DbConnection;
+import Model.Organizer;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jdk.internal.org.jline.utils.Colors.s;
+
 public class OrganizerDAO {
     //    add, getall, getbyid, update
+<<<<<<< HEAD
   public void addOrganizer(Organizer o) throws SQLException {
         try {
             String query =" insert into Organizer (organizer_id, name, email)  Values (?,?,?)";
@@ -12,11 +25,49 @@ public class OrganizerDAO {
 
                 PreparedStatement myQuery = myConnection.prepareStatement(query);
                 myQuery.setInt(1,o.getOrganizerId());
+=======
+
+    public List<Organizer> addOrganizer(Organizer o) throws SQLException {
+        try {
+            String query =" insert into Organizer (organizer_id, name, email)  Values (?,?,?)";
+
+            Connection myConnetion = DbConnection.getConnection();
+            if(myConnetion!=null)
+            {
+                try{
+                    PreparedStatement statement = myConnetion.prepareStatement(query);
+                    ResultSet organizerResults= statement.executeQuery(query);
+                    List<Organizer> allOrganizers = new ArrayList<>();
+
+                    while(organizerResults.next())
+                    {
+                        String id = organizerResults.getString("organizer_id");
+                        String name= organizerResults.getString("name");
+                        String email= organizerResults.getString("email");
+
+                        Organizer stud =new Organizer(o.getOrganizerId(), o.getName(), o.getEmail());
+                        allOrganizers.add(stud);
+                    }
+                    return allOrganizers;
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                PreparedStatement myQuery = myConnetion.prepareStatement(query);
+                myQuery.setString(1,o.getOrganizerId());
+>>>>>>> 9c02176 (saving current AOOP local changes i made)
                 myQuery.setString(2,o.getName());
                 myQuery.setString(3,o.getEmail());
                 myQuery.executeUpdate();
 
+<<<<<<< HEAD
                 System.out.println("Organizer added successfully");
+=======
+
+
+
+>>>>>>> 9c02176 (saving current AOOP local changes i made)
             }
 
         }
@@ -24,6 +75,7 @@ public class OrganizerDAO {
         {
             e.printStackTrace();
         }
+<<<<<<< HEAD
     }
 
     public List<Organizer> getAllOrganizers(Organizer o)
@@ -149,4 +201,10 @@ public class OrganizerDAO {
             e.printStackTrace();
         }
     }
+=======
+        return null;
+
+    }
+
+>>>>>>> 9c02176 (saving current AOOP local changes i made)
 }
